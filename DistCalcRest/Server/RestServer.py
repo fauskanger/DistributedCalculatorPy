4a84ccb82616cab3f13d1beb828f13d634e7762c
Torry Tufteland <tufte017@umn.edu>
2015-02-05 15:58:08 +0100

    RestServer

--- DistCalcRest/Server/RestServer.py
+++ DistCalcRest/Server/RestServer.py
@@ -1,10 +1,34 @@
-from flask import Flask
+#!flask/bin/python
+import re
+from flask import Flask, jsonify, make_response, abort
+from enum import Enum
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+Operand = Enum('Operand', 'PLUS MINUS MULTIPLY DIVIDE POWER')
+
+class Expression:
+    op1     = 0
+    operand = Operand.PLUS
+    op2     = 0
+
+
+@app.route('/api/calc/<expression>', methods=['GET'])
+def calculate(expression):
+
+    # Regular expression to validate that the expression is two operators and one operand
+    match = re.search(r"^[0-9]+(\.|,)?[0-9]*[\*|\+|\-|\^][0-9]+(\.|,)?[0-9]*", expression)
+    if not match:
+        abort(400)
+
+    return jsonify({'expression': expression})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
